6c4490439a59fdb4bc7301297b1e4c49d2be3b56
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2022-06-14 13:30:56 +0900

    Update 로또최고순위_최저순위.py
    프로그래머스 lv.1

--- 22.06.14/로또최고순위_최저순위.py
+++ 22.06.14/로또최고순위_최저순위.py
@@ -29,3 +29,19 @@ def solution(lottos, win_nums):
     answer.append(min_i)
     
     return answer
+
+#또 다른 풀이
+"""
+def solution(lottos, win_nums):
+    num = [6,6,5,4,3,2,1]
+    zero_count = lottos.count(0)
+    count = 0
+    for i in lottos:
+        if i in win_nums:
+            count += 1
+    answer = []
+    answer.append(num[zero_count+count])
+    answer.append(num[count])    
+    
+    return answer
+"""
